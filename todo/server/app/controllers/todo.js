74521630c21c2512da93962b0f504c7da1c1059c
Kate Dennert <kdennert@uwm.edu>
2018-10-29 21:26:22 -0500

    MidtermPAdd

--- todo/server/app/controllers/todo.js
+++ todo/server/app/controllers/todo.js
@@ -20,7 +20,7 @@ module.exports = function (app, config) {
 
     router.get('/todos', asyncHandler(async (req, res) => {
         logger.log('info', 'Get all todos');
-        let query = User.find();
+        let query = Todo.find();
         query.sort(req.query.order)
         await query.exec().then(result => {
             res.status(200).json(result);
@@ -28,7 +28,7 @@ module.exports = function (app, config) {
     }));
 
     router.get('/todos/:id', asyncHandler(async (req, res) => {
-        logger.log('info', 'Get user %s', req.params.id);
+        logger.log('info', 'Get todo %s', req.params.id);
         await Todo.findById(req.params.id).then(result => {
             res.status(200).json(result);
         })
@@ -49,7 +49,7 @@ module.exports = function (app, config) {
 
     router.delete('/todos/:id', asyncHandler(async (req, res) => {
         logger.log('info', 'Deleting todo %s', req.params.id);
-        await User.remove({ _id: req.params.id })
+        await Todo.remove({ _id: req.params.id })
             .then(result => {
                 res.status(200).json(result);
             })
