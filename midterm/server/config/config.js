964f603da3f7418725eb38d81586a678edefb876
Kate Dennert <kdennert@uwm.edu>
2018-11-04 11:06:18 -0600

    more modules

--- midterm/server/config/config.js
+++ midterm/server/config/config.js
@@ -5,21 +5,21 @@ var path = require('path'),
 var config = {
     development: {
         root: rootPath,
-        app: { name: 'ClassProject' },
+        app: { name: 'midterm' },
         port: 5000,
         db: 'mongodb://127.0.0.1/helpMe-dev'
     },
 
     test: {
         root: rootPath,
-        app: { name: 'ClassProject' },
+        app: { name: 'midterm' },
         port: 4000,
         db: 'mongodb://127.0.0.1/helpMe-test'
     },
 
     production: {
         root: rootPath,
-        app: { name: 'ClassProject' },
+        app: { name: 'midterm' },
         port: 80,
         db: 'mongodb://127.0.0.1/helpMe'
     }
